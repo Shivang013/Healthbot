1efc0ca0cd104e4246641fa62e0975fc61604202
Shiavng <shivangsingh014@gmail.com>
2026-06-19 23:46:36 +0530

    Updated file structure

--- test_embedding.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-
-embeddings = GoogleGenerativeAIEmbeddings(
-    model="gemini-embedding-2-preview"
-)
-
-print(embeddings.embed_query("Hello world")[:5])
